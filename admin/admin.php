a6de6232b6c3b8da3ea5e0ccde7616aac730bd42
scossar <scossar3@gmail.com>
2017-02-22 23:31:10 -0800

    Add settings fields

--- admin/admin.php
+++ admin/admin.php
@@ -29,6 +29,7 @@ class Options {
 	public function plugin_settings() {
 		$this->options = DiscourseUtilities::get_options();
 
+		// Common
 		add_settings_section( 'wpdc_common_sso_settings_section', __( 'Common Settings', 'wpdc' ), array(
 			$this,
 			'common_settings_details',
@@ -41,6 +42,7 @@ class Options {
 
 		register_setting( 'wpdc_sso_common', 'wpdc_sso_common', array( $this, 'validate_options' ) );
 
+		// Provider
 		add_settings_section( 'wpdc_sso_provider_section', __( 'SSO Provider Settings', 'wpdc' ), array(
 			$this,
 			'sso_provider_settings_details',
@@ -57,10 +59,33 @@ class Options {
 		), 'wpdc_sso_provider', 'wpdc_sso_provider_section' );
 
 		register_setting( 'wpdc_sso_provider', 'wpdc_sso_provider', array( $this, 'validate_options' ) );
+
+		// Client
+		add_settings_section( 'wpdc_sso_client_section', __( 'SSO Client Settings', 'wpdc' ), array(
+			$this,
+			'sso_client_settings_details',
+		), 'wpdc_sso_client' );
+
+		add_settings_field( 'wpdc_sso_client_enabled', __( 'Enable SSO Client', 'wpdc' ), array(
+			$this,
+			'client_enabled_checkbox',
+		), 'wpdc_sso_client', 'wpdc_sso_client_section' );
+
+		add_settings_field( 'wpdc_sso_sync_email', __( 'Sync Users by Email Address', 'wpdc' ), array(
+			$this,
+			'sync_email_checkbox',
+		), 'wpdc_sso_client', 'wpdc_sso_client_section' );
+
+		add_settings_field( 'wpdc_redirect_client_login', __( 'Redirect from the login page to Discourse', 'wpdc' ), array(
+		        $this,
+            'redirect_login_checkbox',
+        ), 'wpdc_sso_client', 'wpdc_sso_client_section' );
+
+		register_setting( 'wpdc_sso_client', 'wpdc_sso_client', array( $this, 'validate_options' ) );
 	}
 
 	public function add_sso_submenu_page() {
-		add_submenu_page(
+		$sso_settings =  add_submenu_page(
 			'wp_discourse_options',
 			__( 'Single Sign On', 'wpdc' ),
 			__( 'Single Sign On', 'wpdc' ),
@@ -68,6 +93,7 @@ class Options {
 			'wpdc_sso_options',
 			array( $this, 'sso_options_tab' )
 		);
+		add_action( 'load-' . $sso_settings, array( $this->options_page, 'connection_status_notice' ) );
 	}
 
 	public function sso_options_tab() {
@@ -90,16 +116,16 @@ class Options {
 			?>
             <h3 class="nav-tab-wrapper">
                 <a href="?page=wp_discourse_options&tab=sso_common&parent_tab=wpdc_sso_options"
-                   class="nav-tab <?php echo 'sso_common' === $tab ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab <?php echo 'sso_common' === $tab || 'wpdc_sso_options' === $tab ? 'nav-tab-active' : ''; ?>">
 					<?php esc_html_e( 'Common Options', 'wpdc' ); ?>
-                </a>
-                <a href="?page=wp_discourse_options&tab=sso_client&parent_tab=wpdc_sso_options"
-                   class="nav-tab <?php echo 'sso_client' === $tab ? 'nav-tab-active' : ''; ?>">
-					<?php esc_html_e( 'SSO Client', 'wpdc' ); ?>
                 </a>
                 <a href="?page=wp_discourse_options&tab=sso_provider&parent_tab=wpdc_sso_options"
                    class="nav-tab <?php echo 'sso_provider' === $tab ? 'nav-tab-active' : ''; ?>">
 					<?php esc_html_e( 'SSO Provider', 'wpdc' ); ?>
+                </a>
+                <a href="?page=wp_discourse_options&tab=sso_client&parent_tab=wpdc_sso_options"
+                   class="nav-tab <?php echo 'sso_client' === $tab ? 'nav-tab-active' : ''; ?>">
+					<?php esc_html_e( 'SSO Client', 'wpdc' ); ?>
                 </a>
             </h3>
 			<?php
@@ -120,8 +146,21 @@ class Options {
 		$this->input_helper->text_input( 'wpdc-login-path', 'wpdc_sso_provider', __( 'The path to your WordPress login page.', 'wpdc' ) );
 	}
 
+	// SSO Client
+	public function client_enabled_checkbox() {
+		$this->input_helper->checkbox_input( 'wpdc-enable-sso-client', 'wpdc_sso_client', __( 'Enable WordPress to function as an SSO client for Discourse.', 'wpdc' ) );
+	}
+
+	public function sync_email_checkbox() {
+		$this->input_helper->checkbox_input( 'wpdc-sync-client-email', 'wpdc_sso_client', __( 'Sync existing accounts through their email address.', 'wpdc' ) );
+	}
+
+	public function redirect_login_checkbox() {
+		$this->input_helper->checkbox_input( 'wpdc-redirect-client-login', 'wpdc_sso_client', __( 'Redirect the WordPress login path to Discourse.', 'wpdc' ) );
+	}
+
 	public function sso_settings_fields( $tab ) {
-		if ( 'sso_common' === $tab ) {
+		if ( 'sso_common' === $tab || 'wpdc_sso_options' === $tab ) {
 			settings_fields( 'wpdc_sso_common' );
 			do_settings_sections( 'wpdc_sso_common' );
 		}
@@ -130,6 +169,11 @@ class Options {
 			settings_fields( 'wpdc_sso_provider' );
 			do_settings_sections( 'wpdc_sso_provider' );
 		}
+
+		if( 'sso_client' === $tab ) {
+		    settings_fields( 'wpdc_sso_client' );
+		    do_settings_sections( 'wpdc_sso_client' );
+        }
 	}
 
 	public function common_settings_details() {
@@ -141,6 +185,12 @@ class Options {
 	public function sso_provider_settings_details() {
 		?>
         <p>Use your WordPress site as the SSO provider for your forum.</p>
+		<?php
+	}
+
+	public function sso_client_settings_details() {
+		?>
+        <p>Login to WordPress through Discourse.</p>
 		<?php
 	}
 
